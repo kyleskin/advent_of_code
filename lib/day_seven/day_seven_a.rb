7be11befd3b85b831f5c7d4819580f511e1f4c6e
Kyle Skinner <KyleSkinner@Kathleen-Skinners-iMac.local>
2016-12-11 19:05:35 -0700

    day seven part one

--- lib/day_seven/day_seven_a.rb
+++ lib/day_seven/day_seven_a.rb
@@ -1,35 +1,42 @@
 #!usr/bin/evn ruby
 
-def abba_checker(letters)
+def palindrome?(letters)
   letters = letters.chars
   while letters.length > 3 do
-      return true if letters[0..3] == letters[0..3].reverse
+      return true if letters[0..3] == letters[0..3].reverse && letters[0] != letters[1]
       letters.shift
   end
 end
 
-
-def line_valid?(line)
-  bracket_check = []
+def bracket_valid?(line)
   brackets = line.scan(/\[\w+\]/)
-  brackets.each do |item|
-     unless abba_checker(item)
-      bracket_check.push(line).uniq!
-     end
-  end
-  bracket_check.each do |item|
-    non_brackets = item.chomp("\n").split(/\[\w+\]/)
-    non_brackets.each do |entry|
-      @valid.push(line).uniq! if abba_checker(entry)
+  brackets.each do |bracket_contents|
+    if palindrome?(bracket_contents) == true
+      @bracket_palindrome = true
     end
   end
 end
 
-
-File.open('lib/day_seven/day_seven.input') do |file|
-  @valid = []
-  file.each_line do |line|
-    @line = line_valid?(line)
+def outside_brackets_valid?(line)
+  outside_brackets = line.chomp("\n").split(/\[\w+\]/)
+  outside_brackets.each do |outside_contents|
+    if palindrome?(outside_contents) == true
+      @outside_bracket_palindrome = true
+    end
   end
 end
- p @valid.length
+
+File.open('lib/day_seven/day_seven.input') do |file|
+  @count = 0
+  file.each_line do |line|
+    @bracket_palindrome = false
+    @outside_bracket_palindrome = false
+    bracket_valid?(line)
+    if @bracket_palindrome == false
+      outside_brackets_valid?(line)
+      @outside_bracket_palindrome
+    end
+    @count += 1 if @outside_bracket_palindrome == true
+  end
+end
+p @count
